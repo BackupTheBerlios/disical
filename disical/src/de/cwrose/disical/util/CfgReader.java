ffb9915f59c0ff5140cc14f70f0523faf6a4f8ce
LuckyStarr <LuckyStarr>
2002-01-01 09:50:57 +0000

    Kommentar in CfgReader geschrieben, bitte ansehen

--- disical/src/de/cwrose/disical/util/CfgReader.java
+++ disical/src/de/cwrose/disical/util/CfgReader.java
@@ -10,7 +10,7 @@ import java.util.*;
  * as the java vm.
  * 
  * @author Stefan Plantikow <stepn@users.berlios.de>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  * @see java.lang.System#getProperty
  * 
  */
@@ -26,7 +26,46 @@ public final class CfgReader
 
 	 static 
 	 {
-		  String mainStr = System.getProperty (MAIN_DIR);
+                 /* Verstgeh ich nicht, gibt meiner Meinung nach immer null zurück, da es  
+                  * Da es das System Property "disical.cfg" nicht gibt.
+                  *
+                  * Oder versteh ich System.getProperty falsch?
+                  * Soweit ich das verstehe kann man damit nur ganz bestimmte System Properties, 
+                  * wie die jvm (java.vm.name) oder das Working Directory bestimmen 
+                  * (user.dir).
+                  *
+                  * Soweit ich weiss gibt es auch ein Config File fuer die JVM, um 
+                  * solche properties zu setzen, aber dazu finde ich gerade nichts im
+                  * javadoc und die Frage ist auch ob wir das wollen
+                  *
+                  * Ansonsten finde ich die Idee einer Zentralen Classe zum verwalten
+                  * der Configuration sehr gut, auch dass man die Configuration ueber 
+                  * eine statische Methode bekommt.
+                  * Ich habe mir darueber auch schon gedanken gemacht und mir einige 
+                  * Compliziertere möglichkeiten angschaut, ueber jndi (PASX Framework)
+                  * , aber da ich da bis jetzt noch nicht so ganz durchsteige, glaube 
+                  * ich langsam, das man sich damit noch mehr ärger macht als man sich 
+                  * vorteile verschafft.
+                  *
+                  * Ein vorschlag von mir wäre noch, dem CfgReader eine Statische 
+                  * Methode hinzuzufuegen, die ein fuer DiSiCal configuriertes Castor 
+                  * Database Object zurückgibt. Das wuerde es ermöglichen, dass Objekte
+                  * selber auf das Persistenz layer zugreifen koennen, um sich selbst 
+                  * zu persistieren. Überhaupt haette man so einen einfachen zugriff 
+                  * auf Castor. Das löst  meiner Erfahrung nach manche Probleme mit Castor, 
+                  * insbesondere beim erzeugen des Database Objekts und dem Einlesen der
+                  * xml cofigurations Dateien database.xml oder mapping.xml.
+                  *
+                  * Ups, eigentlich sollte das nur eine Frage zum Code werden, ich glaube
+                  * ich verlege weitere Design & Planungsaktivitäten auf das Forum.
+                  * bitte löscht diesen commentar einfach wieder, wenn er nicht mehr 
+                  * aktuell ist.
+                  *
+                  * Gruss Conny
+                  *
+                  */
+                  
+                  String mainStr = System.getProperty (MAIN_DIR);
 		  mainDir = new File (mainStr);
 
 		  String localStr = System.getProperty (LOCAL_DIR);
