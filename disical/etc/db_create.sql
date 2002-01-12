1b9e203edf51c3414f9ccc76b5935456b6a8b95a
stepn <stepn>
2002-01-12 18:07:22 +0000

    -

--- disical/etc/db_create.sql
+++ disical/etc/db_create.sql
@@ -1,24 +1,23 @@
 create table auth (
-	login		varchar ( 16 ),
-	password 	varchar ( 16 ),
-	name		varchar ( 32 ),
-	email		varchar ( 32 ),
-	isAdmin		boolean,
+	login     varchar ( 16 ),
+	password  varchar ( 16 ),
+	name      varchar ( 32 ),
+	email     varchar ( 32 ),
+	isAdmin   boolean,
 	
 	primary key ( login ),
-	unique ( email )
 );
-create unique index auth_email_idx on auth ( email );
+create index auth_email_idx on auth ( email );
 
 create table appointments (
-	id			serial,
-	login		varchar ( 16 ),
-	startDate	date,
-	startTime	time,
-	endDate		date,
-	endTime		time,
-	subject		varchar ( 32 ),
-	location	varchar ( 16 ),
+	id          serial,
+	login       varchar ( 16 ),
+	startDate   date,
+	startTime   time,
+	endDate     date,
+	endTime     time,
+	subject     varchar ( 32 ),
+	location    varchar ( 16 ),
 	
 	primary key ( id )
 );
