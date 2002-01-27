b8875f0baa62fcb0bea9292e96ee2e708b33a566
deafman <deafman>
2002-01-27 15:57:05 +0000

    *** empty log message ***

---
+++ disical/src/de/cwrose/disical/corba/DisicalInvStatus.java
@@ -0,0 +1,14 @@
+package de.cwrose.disical.corba;
+
+public class DisicalInvStatus {
+
+	private short status;
+
+	public short getStatus() {
+		return status;
+	}
+
+	public void setStatus(short status) {
+		this.status = status;
+	}
+}
