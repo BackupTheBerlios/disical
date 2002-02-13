fa8ea20c008398b2bf39c77aeb4e0502d850c7ef
stepn <stepn>
2002-02-13 17:06:01 +0000

    blow() in DbPersistable

--- disical/src/de/cwrose/disical/db/DbPersistable.java
+++ disical/src/de/cwrose/disical/db/DbPersistable.java
@@ -9,62 +9,88 @@ import java.lang.reflect.InvocationTargetException;
 import org.exolab.castor.jdo.DataObjectAccessException;
 
 public class DbPersistable {
+	/* Flag that stores whether a DbPersistable must be created 
+	   or updated when persisted to the db */
 	private boolean create_persistable = true;
 
+	/* Maps CORBA.Object -> bubble */
 	private static Hashtable reverseMapping;
 
 	static
 	{
+		// Initialize Hashes 
 		reverseMapping = new Hashtable ();
 	}
-
+	
 	public DbPersistable () {
-		//		HackHelper.printStackTrace (System.out, "Constructing Persistable");
 	}
 
+
+
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Put bubble p under entry o 
+     * @throws PersistenceException on overwrite
+     */
 	protected static void putBubble(org.omg.CORBA.Object o, DbPersistable p)
 	throws PersistenceException
     {
-		//		System.out.println ("DbPersistable.putBubble: "+o+"("+o.getClass().toString()+")"+"="+p);
-		//HackHelper.printStackTrace (System.out, "PUT_BUBBLE");
 		if (reverseMapping.containsKey (o))
 			throw new PersistenceException 
 				("Attempt to overwrite bubble for CORBA object");
 		reverseMapping.put (o, p);
 	}
 
+
+
+	/**
+     * Update bubble p under entry p
+     * @throws PersistenceException if not updateabnle 
+	 */
 	protected static void updateBubble(org.omg.CORBA.Object o, DbPersistable p)
     {
-		//System.out.println ("DbPersistable.updBubble: "+o+"("+o.getClass().toString()+")"+"="+p);
-		//HackHelper.printStackTrace (System.out, "PUT_BUBBLE");
 		reverseMapping.put (o, p);
-		//HackHelper.printStackTrace (System.out, "UPDATE_BUBBLE");
-	}
+    }
 
-	protected static DbPersistable lookupBubble(org.omg.CORBA.Object o)
-	throws PersistenceException
-	{
+
+
+	/**
+     * Lookup bubble for entry o
+     * @throws PersistenceException if not found
+	 */
+    protected static DbPersistable lookupBubble(org.omg.CORBA.Object o)
+	throws PersistenceException {
 		DbPersistable p = null;
-		//System.out.println ("DbPersistable.lookupBubble: "+o+"("+o.getClass().toString()+")");
-		//HackHelper.printStackTrace (System.out, "PUT_BUBBLE");
-		if (!reverseMapping.containsKey (o))
-		{
+
+		if (!reverseMapping.containsKey (o)) {
 			Exception e = new PersistenceException 
 				("Bubble not found for exsiting	CORBA object");
 			e.printStackTrace(System.out);
 		}
-		try
-		{
+		try	{
 			p = (DbPersistable)reverseMapping.get(o);
-		    //System.out.println ("DbPersistable.retBubble: "+p);
+			//System.out.println ("DbPersistable.retBubble: "+p);
 			return p;
 		}
-		catch (ClassCastException e)
-		{
+		catch (ClassCastException e) {
 			throw new PersistenceException 
-		("Non-DbPersistable registered as bubble for CORBA object.  Ouch.");
+				("Non-DbPersistable registered as bubble for CORBA " +
+				 "object.  Ouch.");
 		}
 	}
+
+
+
+
+	// ------------------------------------------------------------------------
+
+    protected void blow (org.omg.CORBA.Object obj) 
+    throws PersistenceException 
+    {
+		putBubble (obj, this);
+    }
 
 	public final void growOld ()
 	{
